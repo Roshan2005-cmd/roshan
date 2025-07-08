5e10ec3d82936743e1435f54c40f8211ded24193
Roshan Pansheriya <Roshan Pansheriya@DESKTOP-F4BNL5J>
2025-07-08 19:18:10 -0400

    Update code

---
+++ PermitManagementSystem/src/permitmanagementsystem/PermitCategory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package permitmanagementsystem;
+
+/**
+ *
+ * @author Roshan Pansheriya
+ */
+public enum PermitCategory {
+    GENERAL,
+    STUDENT,
+    STAFF,
+    ROSHAN_CATEGORY
+}
---
+++ PermitManagementSystem/src/permitmanagementsystem/PermitManagementSystem.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package permitmanagementsystem;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Roshan Pansheriya
+ */
+public class PermitManagementSystem {
+
+    public static boolean isValidPermit(String permitNumber) {
+        return permitNumber.matches("\\d{6}");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String permitNumber;
+        
+        while (true) {
+            System.out.print("Enter a 6-digit permit number: ");
+            permitNumber = sc.nextLine();
+            if (isValidPermit(permitNumber)) {
+                break;
+            } else {
+                System.out.println("Invalid format. Please enter exactly 6 digits.");
+            }
+        }
+
+        System.out.println("\nChoose a Permit Category:");
+        for (PermitCategory category : PermitCategory.values()) {
+            System.out.println("- " + category);
+        }
+        
+        System.out.print("Enter category name (e.g., GENERAL, STUDENT): ");
+        String inputCategory = sc.nextLine().toUpperCase();
+
+        try {
+            PermitCategory selected = PermitCategory.valueOf(inputCategory);
+
+            Permit permit = new Permit(permitNumber, selected);
+            System.out.println("\nPermit created successfully!");
+            System.out.println(permit);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid category. Program exiting.");
+        }
+
+        sc.close();
+    }
+}
